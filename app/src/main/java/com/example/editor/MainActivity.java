b2237b2f8bebb1d3ef6f696a478ffe931172cc15
Lynx <2104934038@qq.com>
2019-09-11 21:52:28 +0800

    button clicklistener

--- app/src/main/java/com/example/editor/MainActivity.java
+++ app/src/main/java/com/example/editor/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.editor;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.content.Context;
+import android.animation.ObjectAnimator;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
     CloseDialog closeDialog;// 0
@@ -19,25 +25,18 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     ManagerLow managerLow;
     ManagerMid managerMid;
 
+    int button_move = 280;// 底端button平移距离
+    final int button_id = 1234321;// 防id冲突
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 总初始化
-        initMain();
-
-        // 检查权限
-        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
-        int check_result = ActivityCompat.checkSelfPermission(this, permission);// `允许`返回0,`拒绝`返回-1
-        if (check_result != PackageManager.PERMISSION_GRANTED) {// 没有`写`权限
-            ActivityCompat.requestPermissions(this, new String[]{permission}, 1);// 获取`写`权限
-        }
+        initParam();
     }
 
-    public void initMain() {// 初始化窗口和功能函数
-
-
+    public void initParam() {// 初始化窗口和功能函数
         // 初始化窗口
         closeDialog = new CloseDialog();
         editDialog = new EditDialog();
@@ -49,6 +48,55 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         // 初始化功能函数
         managerLow = new ManagerLow(MainActivity.this);
         managerMid = new ManagerMid(MainActivity.this);
+
+        // 检查权限
+        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+        int check_result = ActivityCompat.checkSelfPermission(this, permission);// `允许`返回0,`拒绝`返回-1
+        if (check_result != PackageManager.PERMISSION_GRANTED) {// 没有`写`权限
+            ActivityCompat.requestPermissions(this, new String[]{permission}, 1);// 获取`写`权限
+        }
+    }
+
+    public void initButton() {
+        // 控制`隐藏/显示`按钮
+        Button btnCtrl = findViewById(R.id.ctrlButton);
+        btnCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+            }
+        });
+
+        // `新建`按钮
+        Button btnNew = findViewById(R.id.newButton);
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        // `打开`按钮
+        Button openBtn = findViewById(R.id.openButton);
+        openBtn.setOnClickListener(new View.OnClickListener() {// 点击`打开`按钮
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        // `保存`按钮
+        Button btnSave = findViewById(R.id.saveButton);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        // `关闭`按钮
+        Button closeBtn = findViewById(R.id.closeButton);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 
     @Override
