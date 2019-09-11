014eafeef70745319b6d99981180588119263857
Lynx <2104934038@qq.com>
2019-09-11 21:31:53 +0800

    5 window

--- app/src/main/java/com/example/editor/MainActivity.java
+++ app/src/main/java/com/example/editor/MainActivity.java
@@ -6,15 +6,50 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
+    CloseDialog closeDialog;// 0
+    EditDialog editDialog;// 1
+    NewManager newManager;// 2
+    OpenManager openManager;// 3
+    SaveManager saveManager;// 4
+    int window_num;// 调用哪个窗口
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        closeDialog = new CloseDialog();
+        editDialog = new EditDialog();
+        newManager = new NewManager();
+        openManager = new OpenManager();
+        saveManager = new SaveManager();
+        window_num = -1;
     }
 
     @Override
     public void onDismiss(DialogInterface dialogInterface) {
-        ;
+        if (window_num == 0) {
+            ;
+        }
+    }
+
+    public int resClose() {
+        return -1;
+    }
+
+    public int resEdit() {
+        return -1;
+    }
+
+    public int resNew() {
+        return -1;
+    }
+
+    public int resOpen() {
+        return -1;
+    }
+
+    public int resSave() {
+        return -1;
     }
 }
