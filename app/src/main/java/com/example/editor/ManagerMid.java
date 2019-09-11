f6b3a284c450ffcff4a89128a1ac800446790f16
Lynx <2104934038@qq.com>
2019-09-11 21:55:43 +0800

    newTempFile

--- app/src/main/java/com/example/editor/ManagerMid.java
+++ app/src/main/java/com/example/editor/ManagerMid.java
@@ -14,4 +14,16 @@ public class ManagerMid {
     public int loadTempFile(String tempPath) {// 将临时文件加载到tab
         return -1;
     }
+
+    public int unlinkTempFile(String tempPath) {// 从磁盘中删除文件并解绑
+        return -1;
+    }
+
+    public int closeTab() {// 关闭当前窗口
+        return -1;
+    }
+
+    public String newTempFile() {// 在app目录新建临时文件
+        return null;
+    }
 }
