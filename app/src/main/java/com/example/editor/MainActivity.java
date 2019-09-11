048db8139b130fd57e723d7ed9db61c216a56999
Lynx <2104934038@qq.com>
2019-09-11 21:20:35 +0800

    closeWindow

--- app/src/main/java/com/example/editor/MainActivity.java
+++ app/src/main/java/com/example/editor/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.editor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialogInterface) {
+        ;
     }
 }
---
+++ app/src/main/java/com/example/editor/closeWindow.java
@@ -0,0 +1,82 @@
+package com.example.editor;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+public class closeWindow extends DialogFragment {
+    public Button yes;
+    public Button cancel;
+    public Button no;
+    public int result;
+
+    @Override
+    public void show(FragmentManager fragmentManager, String tag) {
+        super.show(fragmentManager, tag);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i("fuck", "on create view");
+        View view = inflater.inflate(R.layout.layout_close, container);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(0x00000000));// 背景透明
+
+        // 绑定按钮事件
+        initButton(view);
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_FRAME, android.R.style.Theme);// 关闭背景(点击外部不能取消)
+    }
+
+    private void initButton(View view) {
+        yes = view.findViewById(R.id.yes_button);
+        cancel = view.findViewById(R.id.cancel_button);
+        no = view.findViewById(R.id.no_button);
+
+        yes.setOnClickListener(new View.OnClickListener() {//
+            @Override
+            public void onClick(View view) {
+                result = 1;
+                dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result = 0;
+                dismiss();
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result = -1;
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onDismiss(final DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Activity activity = getActivity();
+        if (activity instanceof DialogInterface.OnDismissListener) {
+            ((DialogInterface.OnDismissListener) activity).onDismiss(dialog);
+        }
+    }
+}
