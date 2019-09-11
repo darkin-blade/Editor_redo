8dcfad797540f543293a8b381da00795e717ef28
Lynx <2104934038@qq.com>
2019-09-11 19:21:07 +0800

    ManagerLow

---
+++ app/src/main/java/com/example/editor/ManagerLow.java
@@ -0,0 +1,29 @@
+package com.example.editor;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.EditText;
+
+public class ManagerLow {
+    Context context;
+
+    public void ManagerLow(Context context) {
+        this.context = context;
+    }
+
+    public int unlinkTempFile(String tempPath) {// 删除临时文件,并解除其文件绑定
+        SharedPreferences pFile = context.getSharedPreferences("tab", Context.MODE_PRIVATE);
+        SharedPreferences.Editor eFile = pFile.edit();
+        eFile.putString(tempPath, null);// 解除临时文件与文件的绑定
+        // TODO
+        return -1;
+    }
+
+    public int readFile(String path, EditText text) {
+        return -1;
+    }
+
+    public int writeFile(String path, EditText text) {
+        return -1;
+    }
+}
---
+++ app/src/main/java/com/example/editor/ManagerMid.java
@@ -0,0 +1,17 @@
+package com.example.editor;
+
+import android.content.Context;
+
+import java.io.File;
+
+public class ManagerMid {
+    Context context;
+
+    public void ManagerMid(Context context) {
+        this.context = context;
+    }
+
+    public int loadTempFile(String tempPath) {
+        return -1;
+    }
+}
