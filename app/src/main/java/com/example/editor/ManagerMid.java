ec92609a4d3e8d9753766b5e7124831126adacb9
Lynx <2104934038@qq.com>
2019-09-13 10:56:34 +0800

    closeTab

--- app/src/main/java/com/example/editor/ManagerMid.java
+++ app/src/main/java/com/example/editor/ManagerMid.java
@@ -83,6 +83,37 @@ public class ManagerMid extends ManagerLow {
     }
 
     public int closeTab() {// 关闭当前窗口
+        if (MainActivity.cur_num == -1) {// TODO 提示
+            return 1;
+        }
+
+        // 删除当前tab
+        Button btn = view.findViewById(MainActivity.cur_num + MainActivity.button_id);
+        LinearLayout layout = view.findViewById(R.id.file_tab);// 窗口栏
+        layout.removeView(btn);
+
+        // 移动其余的tab
+        String tempPath = null;
+        SharedPreferences pTab = context.getSharedPreferences("tab", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pTab.edit();
+        if (MainActivity.cur_num + 2 <= MainActivity.total_num) {// 不是最后一个tab,且当前总tab >= 2
+            for (int i = MainActivity.cur_num + 1; i < MainActivity.total_num ; i ++) {
+                // 修改tab与临时文件的绑定
+                tempPath = pTab.getString(i + "", null);// TODO 必须非空
+                editor.putString((i - 1) + "", tempPath);
+
+                // 修改tab的id
+                btn = view.findViewById(i + MainActivity.button_id);
+                btn.setId(i - 1 + MainActivity.button_id);
+            }
+        } else {// 是最后一个tab TODO 不管当前打开文件的数目
+            editor.putString(MainActivity.cur_num + "", null);// 解除tab的绑定
+            MainActivity.cur_num --;// TODO 会现将该窗口置为不活跃
+        }
+        editor.commit();
+        MainActivity.total_num --;
+        changeTab(MainActivity.cur_num);// 切换至临近窗口
+
         return -1;
     }
 
